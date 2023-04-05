070559f8aed3c23e662484f3c998efafe4c4aa0c
MatiSal <salinasmatias100@gmail.com>
2023-04-05 09:10:45 -0300

    Add: kataEntity

---
+++ src/domain/entities/kata.entity.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const kataEntity = () => {
+    let kataSchema = new mongoose.Schema(
+        {
+            name: String,
+            description: String,
+            level: Number,
+            user: Number,
+            date: Date,
+            valoration: Number,
+            chances: Number
+        }
+    )
+    return mongoose.model('Katas', kataSchema)
+}
